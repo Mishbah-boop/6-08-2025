d42ab7cd7467b12d2594e4b5448ee13e28d08e3b
Mishbah-boop <mishbahulkhofid0@gmail.com>
2025-08-06 11:31:30 +0700

    Add files via upload

---
+++ DASAR.java
@@ -0,0 +1,15 @@
+public class DASAR {
+    public static void main(String[] args) {
+       String nama = "Mishbahul khofid";
+       int umur = 15;
+       double beratBadan = 40;
+       char jenisKelamin = 'L';
+       boolean statusSiswa = true;
+
+       System.out.println("Nama  :"+nama);
+       System.out.println("Umur :"+umur);
+       System.out.println("Berat badan :"+ beratBadan);
+       System.out.println("Jenis kelamin :"+jenisKelamin);
+
+    }
+}
