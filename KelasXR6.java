b09d2ee469d25d343c6087c6eca340ef9a1080b8
Mishbah-boop <mishbahulkhofid0@gmail.com>
2025-08-06 11:35:36 +0700

    Add files via upload

---
+++ KelasXR6.java
@@ -0,0 +1,12 @@
+public class KelasXR6 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Mishbahul khofid");
+        System.out.println("Asal Sekolah : MTSN 1 Bojanegoro");
+        System.out.println("Alamat RUmah : Desa NGulanan RT3 RW1 Kec Dander Kab Bojonegoro");
+        System.out.println("Jenis Kelamin : Laki Laki");
+        System.out.println("Cita Cita : Bahagia Dunia Akhirat");
+        System.out.println("Hobi : Bermain/Menonton Sepak Bola");
+
+    }
+
+}
